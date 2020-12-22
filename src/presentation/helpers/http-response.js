a32b8bdddbc02a1b789dc182deaa81758ea55e80
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-22 13:49:59 -0400

    move HttpResponse

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,11 @@
+const MissingParamError = require("../errors/missing-param-error");
+
+module.exports = class HttpResponse {
+  static badRequest(paramName) {
+    return { statusCode: 400, body: new MissingParamError(paramName) };
+  }
+
+  static serverError() {
+    return { statusCode: 500 };
+  }
+};
