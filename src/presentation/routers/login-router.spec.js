65d65f05332ec7ecd523cc44c351958fc6b06533
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-22 14:58:49 -0400

    ensure LoginRouter return 401 when invalid credentials

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -56,4 +56,11 @@ describe("login router", () => {
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
   });
+
+  test("should return 401 when invalid credentials are provided", () => {
+    const { sut } = makeSut();
+    const httpRequest = { body: { email: "invalid", password: "invalid" } };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(401);
+  });
 });
