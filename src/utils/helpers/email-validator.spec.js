65ae1f2f85ee74394dd0cb154e4feddf869a00c1
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-23 16:44:42 -0400

    ensure emailValidator return true if email valid

---
+++ src/utils/helpers/email-validator.spec.js
@@ -0,0 +1,9 @@
+const EmailValidator = require("./email-validator");
+
+describe("Email Validator", () => {
+  test("Should return true if validator returns true", () => {
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid("valid@email.com");
+    expect(isEmailValid).toBe(true);
+  });
+});
