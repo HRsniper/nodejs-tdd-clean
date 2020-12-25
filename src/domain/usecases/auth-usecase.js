b24558b77aef0c5b75edf2dc0e09e9c85b462d0c
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-25 00:22:03 -0400

    ensure AuthUseCase throw MissingParamError if no email

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,10 +1,12 @@
+const { MissingParamError } = require("../../utils/errors");
+
 module.exports = class AuthUseCase {
   async auth(email /* , password */) {
     // this.email = email;
     // this.password = password;
 
     if (!email) {
-      throw new Error();
+      throw new MissingParamError("email");
     }
 
     // return this.accessToken;
--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,10 @@
 const AuthUseCase = require("./auth-usecase");
+const { MissingParamError } = require("../../utils/errors");
 
 describe("Auth UseCase", () => {
   test("Should throw if no email is provided", async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
-    await expect(promise).rejects.toThrow();
+    await expect(promise).rejects.toThrow(new MissingParamError("email"));
   });
 });
