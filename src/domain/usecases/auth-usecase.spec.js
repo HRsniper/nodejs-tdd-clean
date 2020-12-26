52fdb208d8dde44e798900719c1f31f0f93f9f15
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-26 15:09:22 -0400

    catch isValid in AuthUseCase, simplify code with factory helpers

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -2,7 +2,20 @@ const { MissingParamError } = require("../../utils/errors");
 
 const AuthUseCase = require("./auth-usecase");
 
-function makeSut() {
+function makeEncrypter() {
+  class EncrypterSpy {
+    async compare(password, hashedPassword) {
+      this.password = password;
+      this.hashedPassword = hashedPassword;
+      return this.isValid;
+    }
+  }
+  const encrypterSpy = new EncrypterSpy();
+  encrypterSpy.isValid = true;
+  return { encrypterSpy };
+}
+
+function makeLoadUserByEmailRepository() {
   class LoadUserByEmailRepositorySpy {
     async load(email) {
       this.email = email;
@@ -10,17 +23,14 @@ function makeSut() {
       return this.user;
     }
   }
-
-  class EncrypterSpy {
-    async compare(password, hashedPassword) {
-      this.password = password;
-      this.hashedPassword = hashedPassword;
-    }
-  }
-
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
   loadUserByEmailRepositorySpy.user = { password: "hashedPassword" };
-  const encrypterSpy = new EncrypterSpy();
+  return { loadUserByEmailRepositorySpy };
+}
+
+function makeSut() {
+  const { loadUserByEmailRepositorySpy } = makeLoadUserByEmailRepository();
+  const { encrypterSpy } = makeEncrypter();
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy);
   return { sut, loadUserByEmailRepositorySpy, encrypterSpy };
 }
@@ -66,7 +76,8 @@ describe("Auth UseCase", () => {
   });
 
   test("Should return null if invalid password is provided", async () => {
-    const { sut } = makeSut();
+    const { sut, encrypterSpy } = makeSut();
+    encrypterSpy.isValid = false;
     const accessToken = await sut.auth("valid@email.com", "invalid");
     expect(accessToken).toBeNull();
   });
