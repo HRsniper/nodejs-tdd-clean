fb6a95ab339cfdecfd73af8cd9291906176e9a60
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-01-06 16:59:56 -0400

    create LoginRouter composer

--- src/Main/routes/login.routes.js
+++ src/Main/routes/login.routes.js
@@ -1,27 +1,7 @@
-const LoginRouter = require("../../presentation/routers/login-router");
-const AuthUseCase = require("../../domain/usecases/auth-usecase");
-const EmailValidator = require("../../utils/helpers/email-validator");
-const LoadUserByEmailRepository = require("../../infrastructure/repositories/load-user-by-email-repository");
-const UpdateAccessTokenRepository = require("../../infrastructure/repositories/update-access-token-repository");
-const Encrypter = require("../../utils/helpers/encrypter");
-const TokenGenerator = require("../../utils/helpers/token-generator");
-
-const env = require("../config/env");
+const loginRouter = require("../composers/login-router-composer");
 
 module.exports = async (router) => {
-  const loadUserByEmailRepository = new LoadUserByEmailRepository();
-  const updateAccessTokenRepository = new UpdateAccessTokenRepository();
-  const encrypter = new Encrypter();
-  const tokenGenerator = new TokenGenerator(env.tokenSecret);
-
-  const authUseCase = new AuthUseCase({
-    loadUserByEmailRepository,
-    encrypter,
-    tokenGenerator,
-    updateAccessTokenRepository
-  });
-  const emailValidator = new EmailValidator();
-  const loginRouter = new LoginRouter({ authUseCase, emailValidator });
-
   router.post("/login", loginRouter);
 };
+
+// https://youtu.be/pRgnVnsjSjw?t=1786
