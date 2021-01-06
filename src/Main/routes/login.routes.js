4a0a3dbeb3c07fa72ee99da77e4b10a0f1995123
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-01-06 17:07:49 -0400

    create express router adapter

--- src/Main/routes/login.routes.js
+++ src/Main/routes/login.routes.js
@@ -1,7 +1,6 @@
 const loginRouter = require("../composers/login-router-composer");
+const ExpressRouterAdapter = require("../adapters/express-router-adapter");
 
 module.exports = async (router) => {
-  router.post("/login", loginRouter);
+  router.post("/login", ExpressRouterAdapter.adapter(loginRouter));
 };
-
-// https://youtu.be/pRgnVnsjSjw?t=1786
